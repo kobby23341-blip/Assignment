df670d78f8998304957e48b119a0f1ade047cb98
kobby23341-blip <kobby23341@gmail.com>
2026-06-24 21:55:20 +0000

    Add files via upload

---
+++ About_Myself.java
@@ -0,0 +1,31 @@
+public class About_Myself {
+    static void main(String[] args) {
+        //variables
+        String name = "Francis Amakye";
+        int age = 22;
+        String hobby = "Gaming";
+        Float accountBalance = 101101471762F;
+        int shoeSize = 44;
+        double height = 5; // in feet
+        String TelNo = "+233547012367";
+
+        //boolean - true or false only
+        boolean isKTUStudent = true;
+
+
+
+
+        //Output
+
+        System.out.printf("Name: Francis Amakye%n", name);
+        System.out.printf("Age: 22%n",age);
+        System.out.printf("Hobby: Gaming%n",hobby);
+        System.out.printf("Account Balance: 1011101471762%n" , accountBalance);
+        System.out.printf("Shoe Size: 44%n", shoeSize);
+        System.out.printf("Height 5%n", height);
+        System.out.printf("Telephone Number: +233547012367%n", TelNo);
+        System.out.printf("KTU Students: " + isKTUStudent);
+
+
+    }
+}
